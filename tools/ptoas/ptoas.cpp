767799b324bd4104b54a3a56af4f9c6b63e2e6c8
mouliangyu <21963576+mouliangyu@users.noreply.github.com>
2026-06-29 15:43:15 +0800

    Rename VMI layout fold pass

--- tools/ptoas/ptoas.cpp
+++ tools/ptoas/ptoas.cpp
@@ -3010,7 +3010,7 @@ static LogicalResult runVMISemanticPipeline(OwningOpRef<ModuleOp> &module) {
   pm.addPass(pto::createVMILayoutAssignmentPass());
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
-  pm.addPass(pto::createVMILayoutFoldConsumersPass());
+  pm.addPass(pto::createVMILayoutFoldPass());
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
   pm.addPass(pto::createVMILayoutRematerializePass());
