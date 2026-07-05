041f921cf1b20abc82f09be340cbc0994df550d9
mouliangyu <21963576+mouliangyu@users.noreply.github.com>
2026-07-06 00:13:28 +0800

    Refine VMI layout propagation

--- tools/ptoas/ptoas.cpp
+++ tools/ptoas/ptoas.cpp
@@ -3015,6 +3015,7 @@ static LogicalResult runVMISemanticPipeline(OwningOpRef<ModuleOp> &module) {
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
   pm.addPass(pto::createVMILegalizeArithSelectPass());
+  pm.addPass(pto::createVMIMaskGranularityAssignmentPass());
   pm.addPass(pto::createVMILayoutAssignmentPass());
   pm.addPass(createCanonicalizerPass());
   pm.addPass(createCSEPass());
