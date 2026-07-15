3368de8b2717393472f6985d64007952cc12d689
mouliangyu <mouliangyu@huawei.com>
2026-07-16 00:05:51 +0800

    Narrow VPTO loop counters to i16

--- tools/ptoas/ptoas.cpp
+++ tools/ptoas/ptoas.cpp
@@ -2667,6 +2667,8 @@ static void prepareVPTOForEmission(PassManager &pm) {
       pto::createPTOInferVPTOVecScopePass());
   if (enableVMI)
     kernelModulePM.addPass(createLoopInvariantCodeMotionPass());
+  kernelModulePM.addNestedPass<func::FuncOp>(
+      pto::createPTONarrowVPTOLoopCountersPass());
   kernelModulePM.addPass(createCanonicalizerPass());
   kernelModulePM.addPass(createCSEPass());
   kernelModulePM.addPass(pto::createPTOValidateVPTOEmissionIRPass());
