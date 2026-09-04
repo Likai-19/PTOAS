a35c0b50f76dff5f614611dd890f86e971916755
likai00 <3977648331@qq.com>
2026-09-04 09:27:13 +0800

    fix: add PR386 license headers to split-generated internal files

--- lib/PTO/IR/VMIInternal.h
+++ lib/PTO/IR/VMIInternal.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2026 Huawei Technologies Co., Ltd.
+// This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+// CANN Open Software License Agreement Version 2.0 (the "License").
+// Please refer to the License for details. You may not use this file except in compliance with the License.
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+// See LICENSE in the root of the software repository for the full text of the License.
 // Internal shared helper definitions for VMI.
 // This header is internal to lib/PTO/IR and not installed.
 // Each including translation unit gets its own internal copy of these helpers.
